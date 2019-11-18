61a588b6d6427864694107693262fef687eb71d8
nokijp <nokijp@users.noreply.github.com>
2019-11-19 08:23:34 +0900

    refactor HashCode

--- src/Network/ReminderBot/HashCode.hs
+++ src/Network/ReminderBot/HashCode.hs
@@ -20,16 +20,16 @@ instance Read HashCode where
   readsPrec _ s = coerce (readHex s :: [(Word64, String)])
 
 hashCode :: Word64 -> HashCode
-hashCode n = fromInteger $ toInteger n * p
+hashCode n = HashCode $ n * p
 
 hashCodeInv :: HashCode -> Word64
-hashCodeInv n = fromInteger $ toInteger n * pInv
+hashCodeInv (HashCode n) = n * pInv
 
 internalBitSize :: Num a => a
 internalBitSize = 64
 
-p :: Integer
+p :: Word64
 p = 11400714819323198549  -- nearest prime of 2 ^ 64 / φ
 
-pInv :: Integer
+pInv :: Word64
 pInv = 6236490470931210493  -- p * pInv ≡ 1 (mod 2 ^ 64)
