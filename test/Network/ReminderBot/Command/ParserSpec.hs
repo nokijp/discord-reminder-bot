7ce99954a26a282fa408c8a46d01ed11a9109cab
nokijp <nokijp@users.noreply.github.com>
2020-11-21 19:30:42 +0900

    fix nickname issue

--- test/Network/ReminderBot/Command/ParserSpec.hs
+++ test/Network/ReminderBot/Command/ParserSpec.hs
@@ -14,61 +14,77 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "parseCommand" $ do
+  describe "parseMessage" $ do
     context "when given an \"add\" command" $ do
       it "can parse \"hh:mm\"" $
-        parseCommand "add 11:22 abc" `shouldBe` Right (CommandAdd (CommandTimeHM 11 22) "abc")
+        parseMessage "1234" "<@1234>add 11:22 abc" `shouldBe` Just (Right (CommandAdd (CommandTimeHM 11 22) "abc"))
       it "can parse \"MM/DD hh:mm\"" $
-        parseCommand "add 11/22 33:44 abc" `shouldBe` Right (CommandAdd (CommandTimeMDHM 11 22 33 44) "abc")
+        parseMessage "1234" "<@1234>add 11/22 33:44 abc" `shouldBe` Just (Right (CommandAdd (CommandTimeMDHM 11 22 33 44) "abc"))
       it "can parse \"YYYY/MM/DD hh:mm\"" $
-        parseCommand "add 1111/22/33 44:55 abc" `shouldBe` Right (CommandAdd (CommandTimeYMDHM 1111 22 33 44 55) "abc")
+        parseMessage "1234" "<@1234>add 1111/22/33 44:55 abc" `shouldBe` Just (Right (CommandAdd (CommandTimeYMDHM 1111 22 33 44 55) "abc"))
       it "can parse a command whose message contains multibyte characters" $
-        parseCommand "add 11:22 あいう" `shouldBe` Right (CommandAdd (CommandTimeHM 11 22) "あいう")
+        parseMessage "1234" "<@1234>add 11:22 あいう" `shouldBe` Just (Right (CommandAdd (CommandTimeHM 11 22) "あいう"))
       it "can parse a command whose message contains spaces" $
-        parseCommand "add 11:22 a b  c" `shouldBe` Right (CommandAdd (CommandTimeHM 11 22) "a b  c")
+        parseMessage "1234" "<@1234>add 11:22 a b  c" `shouldBe` Just (Right (CommandAdd (CommandTimeHM 11 22) "a b  c"))
       it "can parse a command which contains an extra time information" $
-        parseCommand "add 11:22 33:44 abc" `shouldBe` Right (CommandAdd (CommandTimeHM 11 22) "33:44 abc")
+        parseMessage "1234" "<@1234>add 11:22 33:44 abc" `shouldBe` Just (Right (CommandAdd (CommandTimeHM 11 22) "33:44 abc"))
       it "can parse a command with extra spaces" $
-        parseCommand "  add  11:22     abc  " `shouldBe` Right (CommandAdd (CommandTimeHM 11 22) "abc")
+        parseMessage "1234" "<@1234>  add  11:22     abc  " `shouldBe` Just (Right (CommandAdd (CommandTimeHM 11 22) "abc"))
       it "returns AddArgumentError if the body is empty" $
-        parseCommand "add" `shouldBe` Left AddArgumentError
+        parseMessage "1234" "<@1234>add" `shouldBe` Just (Left AddArgumentError)
       it "returns AddArgumentError if the message is empty" $
-        parseCommand "add 11:22   " `shouldBe` Left AddArgumentError
+        parseMessage "1234" "<@1234>add 11:22   " `shouldBe` Just (Left AddArgumentError)
+      it "can parse nicknames" $
+        parseMessage "1234" "<@!1234>add 11:22 abc" `shouldBe` Just (Right (CommandAdd (CommandTimeHM 11 22) "abc"))
+      it "returns Nothing if the ID does not match" $
+        parseMessage "1234" "<@5678>add 11:22 abd" `shouldBe` Nothing
 
     context "when given a \"ls\" command" $ do
       it "can parse" $
-        parseCommand "ls" `shouldBe` Right CommandListChannel
+        parseMessage "1234" "<@1234>ls" `shouldBe` Just (Right CommandListChannel)
       it "can parse a command with an option" $
-        parseCommand "ls all" `shouldBe` Right CommandListGuild
+        parseMessage "1234" "<@1234>ls all" `shouldBe` Just (Right CommandListGuild)
       it "can parse a command with extra spaces" $
-        parseCommand "  ls   " `shouldBe` Right CommandListChannel
+        parseMessage "1234" "<@1234>  ls   " `shouldBe` Just (Right CommandListChannel)
       it "can parse a command with an option and extra spaces" $
-        parseCommand "  ls   all   " `shouldBe` Right CommandListGuild
+        parseMessage "1234" "<@1234>  ls   all   " `shouldBe` Just (Right CommandListGuild)
       it "returns ListArgumentError if the command has an unexpected argument" $
-        parseCommand "ls xxx" `shouldBe` Left ListArgumentError
+        parseMessage "1234" "<@1234>ls xxx" `shouldBe` Just (Left ListArgumentError)
+      it "can parse nicknames" $
+        parseMessage "1234" "<@!1234>ls" `shouldBe` Just (Right CommandListChannel)
+      it "returns Nothing if the ID does not match" $
+        parseMessage "1234" "<@5678>ls" `shouldBe` Nothing
 
     context "when given a \"rm\" command" $ do
       it "can parse" $
-        parseCommand "rm 1234abc" `shouldBe` Right (CommandRemove $ ScheduleID 0x1234abc)
+        parseMessage "1234" "<@1234>rm 1234abc" `shouldBe` Just (Right (CommandRemove $ ScheduleID 0x1234abc))
       it "can parse a command with extra spaces" $
-        parseCommand "    rm   1234abc    " `shouldBe` Right (CommandRemove $ ScheduleID 0x1234abc)
+        parseMessage "1234" "<@1234>    rm   1234abc    " `shouldBe` Just (Right (CommandRemove $ ScheduleID 0x1234abc))
       it "can parse a number which starts with 0" $
-        parseCommand "rm 0000" `shouldBe` Right (CommandRemove $ ScheduleID 0)
+        parseMessage "1234" "<@1234>rm 0000" `shouldBe` Just (Right (CommandRemove $ ScheduleID 0))
       it "does not crash when given a large number" $
-        parseCommand "rm ffffffffffffffffffffffffffffffffffffffff" `shouldBe` Right (CommandRemove $ ScheduleID 0xffffffffffffffff)
+        parseMessage "1234" "<@1234>rm ffffffffffffffffffffffffffffffffffffffff" `shouldBe` Just (Right (CommandRemove $ ScheduleID 0xffffffffffffffff))
       it "returns RemoveArgumentError if the command has no ID" $
-        parseCommand "rm" `shouldBe` Left RemoveArgumentError
+        parseMessage "1234" "<@1234>rm" `shouldBe` Just (Left RemoveArgumentError)
       it "returns RemoveArgumentError if the command has an empty ID" $
-        parseCommand "rm " `shouldBe` Left RemoveArgumentError
+        parseMessage "1234" "<@1234>rm " `shouldBe` Just (Left RemoveArgumentError)
       it "returns RemoveArgumentError if the command has an extra argument" $
-        parseCommand "rm 1234abc 1234abc" `shouldBe` Left RemoveArgumentError
+        parseMessage "1234" "<@1234>rm 1234abc 1234abc" `shouldBe` Just (Left RemoveArgumentError)
+      it "can parse nicknames" $
+        parseMessage "1234" "<@!1234>rm 1234abc" `shouldBe` Just (Right (CommandRemove $ ScheduleID 0x1234abc))
+      it "returns Nothing if the ID does not match" $
+        parseMessage "1234" "<@5678>rm 1234abc" `shouldBe` Nothing
 
     context "when given an unknown command" $ do
       it "returns UnknownCommandError" $
-        parseCommand "unknown" `shouldBe` Left UnknownCommandError
+        parseMessage "1234" "<@1234>unknown" `shouldBe` Just (Left UnknownCommandError)
       it "returns UnknownCommandError when given a string which starts with \"add\"" $
-        parseCommand "addd 11:22 message" `shouldBe` Left UnknownCommandError
+        parseMessage "1234" "<@1234>addd 11:22 message" `shouldBe` Just (Left UnknownCommandError)
       it "returns UnknownCommandError when given a string which starts with \"ls\"" $
-        parseCommand "lss" `shouldBe` Left UnknownCommandError
+        parseMessage "1234" "<@1234>lss" `shouldBe` Just (Left UnknownCommandError)
       it "returns UnknownCommandError when given a string which starts with \"rm\"" $
-        parseCommand "rmm 1234abc" `shouldBe` Left UnknownCommandError
+        parseMessage "1234" "<@1234>rmm 1234abc" `shouldBe` Just (Left UnknownCommandError)
+      it "can parse nicknames" $
+        parseMessage "1234" "<@!1234>unknown" `shouldBe` Just (Left UnknownCommandError)
+      it "returns Nothing if the ID does not match" $
+        parseMessage "1234" "<@5678>unknown" `shouldBe` Nothing
